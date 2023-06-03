f9da57259401d584bca8666eac67d60e06a892bf
pysal <your_email>
2023-06-03 22:13:33 +0530

    updated

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,10 +3,15 @@ import './Loading.css';
 
 const Loading = () => {
   return (
+    <>
     <div className="loading-container">
       <div className="loading-spinner"></div>
       <div className="loading-text">Loading...</div>
-    </div>
+      <br/>
+      </div>
+      <h3 className='loading-h3'>Please wait as it may take some time to load as the project is hosted for free</h3>
+    </>
+    
   );
 };
 
